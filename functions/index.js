9d092df0159436c46eab6ce32b747816bb1e756c
anacnoda77 <sm1108shin@naver.com>
2023-09-11 17:32:29 +0900

    Refactor. index.js 코드 수정
    디버깅 과정에서 발생한 코드 오류 수정

--- functions/index.js
+++ functions/index.js
@@ -4,19 +4,25 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 const db = admin.firestore();
 
+const { v4 } = require('uuid');
+const uuid = () => {
+    const tokens = v4().split('-')
+    return tokens[2] + tokens[1] + tokens[0] + tokens[3] + tokens[4];
+}
+
 exports.sendNtf_CreateGroup  = functions.region("asia-northeast3").firestore
                                 .document("grouplist/{docId}").onCreate(async (snap, context) => {
     const newvalue = snap.data();
     
     try {
-          for(var user of newvalue.serviceuser) {
-            const userRef = db.collection('userlist').doc(user);
+          for(const user of newvalue.serviceuser) {
+            const userRef = db.collection("userlist").doc(user);
             const userDoc = await userRef.get();
 
             const message = {
               notification: {
                   title: "그룹 생성 알림",
-                  body: newvalue.groupname + " 그룹이 생성되었어요!",
+                  body: newvalue.groupname + " 그룹이 생성되었어요! 그룹에 소속된 사람들을 살펴보세요.",
                 },
                 token: userDoc.data().fcmtoken,
             };
@@ -39,19 +45,24 @@ exports.sendNtf_CreateSettlement  = functions.region("asia-northeast3").firestor
 const newvalue = snap.data();
 const settlmentGroup = newvalue.groupid;
 
+const masterid = newvalue.masteruserid;
+const masterRef = db.collection("userlist").doc(masterid);
+const masterDoc = await masterRef.get();
+const master = masterDoc.data().name;
+
 try {
-      const groupRef = db.collection('grouplist').doc(settlmentGroup);
+      const groupRef = db.collection("grouplist").doc(settlmentGroup);
       const groupDoc = await groupRef.get();
       const serviceusers = groupDoc.data().serviceusers;
 
-      for(var user of serviceusers) {
-        const userRef = db.collection('userlist').doc(user);
+      for(const userid of serviceusers) {
+        const userRef = db.collection("userlist").doc(userid);
         const userDoc = await userRef.get();
 
         const message = {
           notification: {
               title: "정산 생성 알림",
-              body: newvalue.settlementName + " 정산이 생성되었어요!",
+              body: master + " 님이 생성한 " + newvalue.settlementName + " 정산을 확인해보세요.",
             },
             token: userDoc.data().fcmtoken,
         };
@@ -76,20 +87,20 @@ const user = newvalue.serviceuserid;
 const settlement = newvalue.settlementid;
 
 try {
-      const userRef = db.collection('grouplist').doc(user);
+      const userRef = db.collection("grouplist").doc(user);
       const userDoc = await userRef.get();
 
-      const settlementRef = db.collection('settlementlist').doc(settlement);
+      const settlementRef = db.collection("settlementlist").doc(settlement);
       const settlementDoc = await settlementRef.get();
 
       const master = settlementDoc.data().masteruserid;
-      const masterRef = db.collection('userlist').doc(master);
+      const masterRef = db.collection("userlist").doc(master);
       const masterDoc = await masterRef.get();
 
       const message = {
           notification: {
               title: "정산서 전송 알림",
-              body: masterDoc.data().name+ " 님이 보내신 " + settlementDoc.data().settlementname  + " 정산의 정산서가 전송되었어요. 카카오톡을 확인해보세요!",
+              body: masterDoc.data().name+ " 님이 보내신 " + settlementDoc.data().settlementname  + " 정산의 정산서를 확인해보세요!",
             },
             token: userDoc.data().fcmtoken,
         };
@@ -109,31 +120,31 @@ catch (err) {
 
 exports.sendNtf_CheckSent  = functions.region("asia-northeast3").firestore
                             .document("settlementlist/{docId}").onUpdate(async (change, context) => {
-  const prev = change.before.data().checksent; 
-  const prev_map = new Map(prev);
+  const prev = change.before.data();
+  const prevMap = new Map(prev.checksent);
 
-  const now = change.after.data().checksent; 
-  const now_map = new Map(now);
+  const now = change.after.data();
+  const nowMap = new Map(now.checksent);
   
-  const settlementname = change.after.data().settlementname;
+  const settlementname = now.settlementname;
 
-  const master = change.after.data().masteruserid;
-  const masterRef = db.collection('userlist').doc(master);
+  const master = now.masteruserid;
+  const masterRef = db.collection("userlist").doc(master);
   const masterDoc = await masterRef.get();
 
-  for(const [key,value] of now_map) {
-    const prev_value = prev_map[key];
-    if(prev_value != value) {
-      const userRef = db.collection('userlist').doc(key);
+  for(const [key,value] of nowMap) {
+    const prevValue = prevMap[key];
+    if(prevValue != value) {
+      const userRef = db.collection("userlist").doc(key);
       const userDoc = await userRef.get();
 
       try {
 
-        if(prev_value == 0 && value == 1) {
+        if(prevValue == 0 && value == 1) {
           const message = {
             notification: {
                 title: "송금 확인 요청 알림",
-                body: settlementname + "정산: " + userDoc.data().name+ " 님이 보내신 송금을 확인해주세요!",
+                body: settlementname + " 정산: " + userDoc.data().name+ " 님이 보내신 송금을 확인해주세요!",
               },
               token: masterDoc.data().fcmtoken,
           };
@@ -144,11 +155,11 @@ exports.sendNtf_CheckSent  = functions.region("asia-northeast3").firestore
               console.log("Error sending message:", error);
             });
         }
-        else if(prev_value == 1 && value == 2) {
+        else if(prevValue == 1 && value == 2) {
           const message = {
             notification: {
                 title: "송금 재요청 알림",
-                body: settlementname + "정산: " + "회원님의 송금이 반려되었어요. 확인 후 다시 송금해주세요!",
+                body: settlementname + " 정산: " + "회원님의 송금이 반려되었어요. 확인 후 다시 송금해주세요!",
               },
               token: userDoc.data().fcmtoken,
           };
@@ -159,11 +170,11 @@ exports.sendNtf_CheckSent  = functions.region("asia-northeast3").firestore
               console.log("Error sending message:", error);
             });
         }
-        else if(prev_value == 1 && value == 3) {
+        else if(prevValue == 1 && value == 3) {
           const message = {
             notification: {
                 title: "송금 확인 알림",
-                body: settlementname + "정산: " + masterDoc.data().name+ "님이 송금을 컨펌하였어요!",
+                body: settlementname + " 정산: " + masterDoc.data().name+ "님이 송금을 컨펌하였어요!",
               },
               token: userDoc.data().fcmtoken,
           };
@@ -174,9 +185,7 @@ exports.sendNtf_CheckSent  = functions.region("asia-northeast3").firestore
               console.log("Error sending message:", error);
             });
         }
-        else {
-          console.error(err);
-        }
+
       }
       catch (err) {
         console.error(err);
@@ -195,12 +204,12 @@ exports.sendNtf_finishSettlement  = functions.region("asia-northeast3").firestor
   if (prev.isfinished != now.isfinished) {
       try {
 
-        const groupRef = db.collection('grouplist').doc(settlmentGroup);
+        const groupRef = db.collection("grouplist").doc(settlmentGroup);
         const groupDoc = await groupRef.get();
         const serviceusers = groupDoc.data().serviceusers;
 
-        for(var user of serviceusers) {
-            const userRef = db.collection('userlist').doc(user);
+        for(const user of serviceusers) {
+            const userRef = db.collection("userlist").doc(user);
             const userDoc = await userRef.get();
             
             const message = {
