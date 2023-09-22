3fd633e3db6444edfea9e7b444137e498b374ba6
anacnoda77 <sm1108shin@naver.com>
2023-09-23 00:54:54 +0900

    Fix. main.dart 오류 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,5 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-<<<<<<< Updated upstream
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-=======
 import 'package:firebase_core/firebase_core.dart';
 import 'package:groupsettlement2/class/class_settlement.dart';
 import 'package:groupsettlement2/viewmodel/UserViewModel.dart';
@@ -96,122 +89,105 @@ void main() async{
 
 
 class MyApp extends ConsumerWidget {
->>>>>>> Stashed changes
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routerConfig: _router,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<SplashView> createState() => _SplashViewState();
 }
+class _SplashViewState extends State<SplashView> {
+  var messageString = "";
+  ServiceUser me = ServiceUser();
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  void _getMyDeviceToken(ServiceUser user) async {
+    final token = await FirebaseMessaging.instance.getToken();
+    print("내 디바이스 토큰: $token");
+    user.fcmToken = token;
+    user.tokenTimestamp = DateTime.now().millisecondsSinceEpoch;
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void _checkToken(String userid) async {
+    ServiceUser user = await ServiceUser().getUserByUserId(userid);
+    int nowtime = DateTime.now().millisecondsSinceEpoch;
+    if(user.fcmToken == null || (nowtime - user.tokenTimestamp!) / (1000*60*60*24*30) >= 28)
+    {
+      _getMyDeviceToken(user);
+      FireService().updateDoc("userlist", userid, user.toJson());
+    }
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    _getMyDeviceToken(me);
+    // _checkToken(me.serviceUserId!);
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+      RemoteNotification? notification = message.notification;
+
+      if(notification != null) {
+        FlutterLocalNotificationsPlugin().show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          const NotificationDetails(
+            android: AndroidNotificationDetails(
+              'hight_importance_channel', 'high_importance_notification',
+              importance: Importance.max,
+            ),
+          ),
+        );
+        messageString = message.notification!.body!;
+        print("Foreground 메시지 수신: $messageString");
+      }
     });
+
+    Timer(
+      const Duration(seconds: 2),
+          (){
+        context.go("/MainPage");
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    final Size size = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        Center(child: Container(color: Colors.white,),),
+        Center(child: Container(height: 100, width: 100, color: Colors.deepPurpleAccent),),
+        Positioned(
+          bottom: size.height * 0.6,
+          child: Transform.rotate(
+            angle: pi * 3 / 4,
+            child: Container(height:size.width * 1.7, width: size.width, color: color1,),
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Positioned(
+          top: size.height * 0.6,
+          child: Transform.rotate(
+            angle: pi * 3 / 4,
+            child: Container(height:size.width * 1.7, width: size.width, color: color2,),
+          ),
+        ),
+      ],
     );
   }
 }
