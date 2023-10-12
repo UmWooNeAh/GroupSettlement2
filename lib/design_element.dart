0598cd6afd217ffcf3ee125616471a7a0a6c75ee
gunwoo99 <miniapple212@gmail.com>
2023-10-13 02:45:24 +0900

    Feat: Complete Settlement Page생성
    정산 마무리 후 나타나는 화면 생성
    animation 추가
    color element로 카카오톡 색 추가
    settlementPage에서 matching완료 화면으로 넘어가도록 수정

--- lib/design_element.dart
+++ lib/design_element.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 const Color color1 = Color(0xFFFE5F55);
 const Color color2 = Color(0xFF07BEB8);
+const Color color3 = Color(0xFFF7E600);
 const Color colorGrey = Color(0xFFDBDBDB);
--- lib/view/complete_settlement_matching.dart
+++ lib/view/complete_settlement_matching.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:groupsettlement2/design_element.dart';
+import '../viewmodel/SettlementViewModel.dart';
+import 'dart:math';
 
 class CompleteSettlementMatching extends ConsumerStatefulWidget {
   const CompleteSettlementMatching({super.key});
@@ -10,11 +14,220 @@ class CompleteSettlementMatching extends ConsumerStatefulWidget {
 }
 
 class _CompleteSettlementMatchingState
-    extends ConsumerState<CompleteSettlementMatching> {
+    extends ConsumerState<CompleteSettlementMatching>
+    with TickerProviderStateMixin {
+  late final AnimationController _animation;
+  @override
+  void initState() {
+    super.initState();
+
+    _animation = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+      lowerBound: 0,
+      upperBound: 1,
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _animation.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Stack(),
+    Size size = MediaQuery.of(context).size;
+    double leftright = size.width * 1.1;
+    double topbottom = size.height * 0.6;
+    double speed = size.height * 0.3;
+    double rotation = pi / 4.5;
+    Curve curve = Curves.decelerate;
+    Duration duration = const Duration(milliseconds: 0);
+    final provider = ref.watch(stmProvider);
+    return Scaffold(
+      body: Stack(
+        children: [
+          AnimatedBuilder(
+            animation: _animation,
+            builder: (context, child) {
+              return AnimatedPositioned(
+                curve: curve,
+                duration: duration,
+                bottom: topbottom + _animation.value * speed,
+                left:
+                    leftright - _animation.value * speed * (1 / tan(rotation)),
+                child: Transform(
+                  transform: Matrix4.rotationZ(rotation),
+                  origin: const Offset(0, 300),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: color1,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    width: 800,
+                    height: 300,
+                  ),
+                ),
+              );
+            },
+          ),
+          AnimatedBuilder(
+            animation: _animation,
+            builder: (context, child) {
+              return AnimatedPositioned(
+                curve: curve,
+                duration: duration,
+                top: topbottom + _animation.value * speed,
+                right:
+                    leftright - _animation.value * speed * (1 / tan(rotation)),
+                child: Transform(
+                  transform: Matrix4.rotationZ(rotation),
+                  origin: const Offset(800, 0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: color2,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    width: 800,
+                    height: 300,
+                  ),
+                ),
+              );
+            },
+          ),
+          Align(
+            alignment: const Alignment(0, -0.05),
+            child: SizedBox(
+              width: size.width * 0.85,
+              height: 200,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text.rich(
+                    textAlign: TextAlign.center,
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text:
+                              provider.group.groupName ?? "default group Name",
+                          style: const TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        const TextSpan(
+                          text: " 그룹의 정산,",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text.rich(TextSpan(children: [
+                    TextSpan(
+                      text: provider.settlement.settlementName,
+                      style: const TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    const TextSpan(
+                      text: "을 ",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                    ),
+                    const TextSpan(
+                      text: "저장",
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: color1,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    const TextSpan(
+                      text: "했습니다!",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                    ),
+                  ])),
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: const Alignment(0, 0.5),
+            child: SizedBox(
+              height: 200,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    width: size.width,
+                    height: 55,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      style: OutlinedButton.styleFrom(
+                        backgroundColor: color3,
+                        side: const BorderSide(
+                          color: Colors.transparent,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: const Text(
+                        "카카오톡으로 정산서 공유하기",
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(20, 0, 20, 5),
+                    width: size.width,
+                    height: 55,
+                    child: OutlinedButton(
+                      onPressed: () {
+                        context.go('/');
+                      },
+                      style: OutlinedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        side: const BorderSide(
+                          color: colorGrey,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: const Text(
+                        "홈으로 이동하기",
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      context.go('/SettlementPage/SettlementFinalCheckPage');
+                    },
+                    child: const Text(
+                      "정산결과확인하기",
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
