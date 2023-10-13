0dd5cb3e578fceddf0f7896731623c45cb1c5800
anacnoda77 <sm1108shin@naver.com>
2023-10-13 15:59:15 +0900

    Feat.SettlmentCheckViewModel 개선
    -Settlement에 totalPrice 인스턴스 추가하여 정산의 총 합계 금액을 출력할 수 있게 함
    -StmCheck뷰모델의 전체적인 설계를 다시하여 필요한 인스턴스들을 추가하고 받아오는 생성자 코드를 새로 작성함
    -settlementPaper 모델에 db에는 존재하지 않는 userName을 따로 받아와 추가하여 정보를 나타내는데 편리하게 함

--- lib/viewmodel/SettlementCheckViewModel.dart
+++ lib/viewmodel/SettlementCheckViewModel.dart
@@ -1,32 +1,62 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:groupsettlement2/class/class_group.dart';
 import 'package:groupsettlement2/common_fireservice.dart';
+import '../class/class_receipt.dart';
+import '../class/class_receiptitem.dart';
 import '../class/class_settlement.dart';
+import '../class/class_settlementitem.dart';
 import '../class/class_settlementpaper.dart';
+import '../class/class_user.dart';
 
 final stmCheckProvider = ChangeNotifierProvider<SettlementCheckViewModel>(
-        (ref) => SettlementCheckViewModel("54d974c2-ea2a-4998-89a3-6d9cca52db80","8dcca5ca-107c-4a12-9d12-f746e2e513b7"));
+        (ref) => SettlementCheckViewModel("54d974c2-ea2a-4998-89a3-6d9cca52db80",
+        "88f8433b-0af1-44be-95be-608316118fad", "8dcca5ca-107c-4a12-9d12-f746e2e513b7"));
 
 class SettlementCheckViewModel extends ChangeNotifier{
-  Settlement      settlement      = Settlement();
-  SettlementPaper settlementPaper = SettlementPaper();
+  Settlement                          settlement        = Settlement();
+  String?                             groupName;
+  String?                             masterName;
+  Map<String, SettlementPaper>        settlementPapers  = <String, SettlementPaper> {};
+  Map<String, List<SettlementItem>>   settlementItems   = <String, List<SettlementItem>> {};
+  Map<String, Receipt>                receipts          = <String, Receipt> {};
+  Map<String, List<ReceiptItem>>      receiptItems      = <String, List<ReceiptItem>> {};
 
-  SettlementCheckViewModel(String settlementId, String userId) {
-    _settingSettlementCheckViewModel(settlementId, userId);
+  SettlementCheckViewModel(String settlementId, String groupname, String userId) {
+    _settingSettlementCheckViewModel(settlementId, groupname, userId);
   }
 
-  void _settingSettlementCheckViewModel(String settlementId, String userId) async {
+  void _settingSettlementCheckViewModel(String settlementId, String groupname, String userId) async {
     settlement = await Settlement().getSettlementBySettlementId(settlementId);
-    for(var paper in settlement.settlementPapers.entries) {
-      SettlementPaper temp = await SettlementPaper().getSettlementPaperByPaperId(paper.value);
-      if(settlementPaper.serviceUserId == userId) {
-        settlementPaper = temp;
-        break;
-      }
-    }
+    groupName = groupname;
+    ServiceUser muser = await ServiceUser().getUserByUserId(settlement.masterUserId!);
+    masterName = muser.name;
+
+    settlement.settlementPapers.forEach((key, value) async {
+      SettlementPaper stm = await SettlementPaper().getSettlementPaperByPaperId(value);
+      settlementPapers[key] = stm;
+      List<SettlementItem> items = [];
+      stm.settlementItems.forEach((itemid) async {
+        SettlementItem item = await SettlementItem().getSettlementItemBySettlementItemId(itemid);
+        items.add(item);
+      });
+      settlementItems[key] = items;
+    });
+
+    settlement.receipts.forEach((rcpid) async {
+      Receipt receipt = await Receipt().getReceiptByReceiptId(rcpid);
+      receipts[rcpid] = receipt;
+      List<ReceiptItem> items = [];
+      receipt.receiptItems.forEach((itemid) async {
+        ReceiptItem item = await ReceiptItem().getReceiptItemByReceiptItemId(itemid);
+        items.add(item);
+      });
+      receiptItems[rcpid] = items;
+    });
+
     notifyListeners();
   }
-  
+
   void requestCheckMySent(String userId) {
     settlement.checkSent[userId] = 1;
     FireService().updateDoc("settlementlist", settlement.settlementId!, settlement.toJson());
