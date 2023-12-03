423bac76abec616c5babc7a483f3c7c7f197ce4a
akuby21 <39649594+akuby21@users.noreply.github.com>
2023-12-04 03:38:30 +0900

    Refactor : Changed parameter for child widget

--- lib/view/account_detail_page.dart
+++ lib/view/account_detail_page.dart
@@ -15,7 +15,7 @@ class AccountDetailPage extends ConsumerStatefulWidget {
 }
 
 class _AccountDetailPageState extends ConsumerState<AccountDetailPage> {
-
+  int viewDetailIndex = -1;
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
@@ -50,8 +50,16 @@ class _AccountDetailPageState extends ConsumerState<AccountDetailPage> {
           SizedBox(height:size.height*0.05),
           Column(
             children: List.generate(provider.accounts.length, (index){
-              Account account = provider.accounts[index];
-              return oneAccount(accountNum: account.accountNum.toString(),nickName: account.accountAlias!,bankName: account.bank!,isFavorite: index == 0,);
+              return GestureDetector(
+                onTap: (){
+                  setState(() {
+                    viewDetailIndex = viewDetailIndex == index ? -1 : index;
+                  });
+                },
+                  child: oneAccount(
+                    account: provider.accounts[index],
+                    isFavorite: index == 0,
+                    viewDetail: viewDetailIndex == index,));
             })
           ),
 
@@ -63,127 +71,152 @@ class _AccountDetailPageState extends ConsumerState<AccountDetailPage> {
   }
 }
 
-class oneAccount extends StatefulWidget {
-  final String nickName;
-  final String bankName;
-  final String accountNum;
+class oneAccount extends ConsumerStatefulWidget {
+  final Account account;
   final bool isFavorite;
-  const oneAccount({Key? key,required this.nickName, required this.accountNum, required this.bankName,required this.isFavorite}) : super(key: key);
+  final bool viewDetail;
+  const oneAccount({Key? key,required this.account,required this.isFavorite,required this.viewDetail}) : super(key: key);
 
   @override
-  State<oneAccount> createState() => _oneAccountState();
+  ConsumerState<oneAccount> createState() => _oneAccountState();
 }
 
-class _oneAccountState extends State<oneAccount> {
-  bool viewDetail = false;
+class _oneAccountState extends ConsumerState<oneAccount> {
+
   @override
   Widget build(BuildContext context) {
+    bool viewDetail = widget.viewDetail;
     final Size size = MediaQuery.of(context).size;
     double fontsize = widget.isFavorite ? 15 : 14;
-    double detailSize = widget.isFavorite ? size.height*0.22 : size.height*0.2;
-    double simpleSize = widget.isFavorite ? size.height*0.11 : size.height*0.075;
-    return GestureDetector(
-      onTap:(){
-        setState(() {
-          viewDetail = !viewDetail;
-        });
-      },
-      child: AnimatedContainer(
-        duration: Duration(milliseconds: 10),
-        height: viewDetail ? detailSize : simpleSize,
-        child: SingleChildScrollView(
-          physics: NeverScrollableScrollPhysics(),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                widget.isFavorite ? Text("대표 계좌",
-                  style: TextStyle(
-                    color: color1,
-                    fontSize:20,
-                    fontWeight: FontWeight.w600
+    double detailSize = widget.isFavorite ? size.height*0.23 : size.height*0.18;
+    double simpleSize = widget.isFavorite ? size.height*0.11 : size.height*0.08;
+    final provider = ref.watch(userProvider);
+    return AnimatedContainer(
+      curve: Curves.fastEaseInToSlowEaseOut,
+      duration: Duration(milliseconds: 500),
+      height: viewDetail ? detailSize : simpleSize,
+      child: SingleChildScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  widget.isFavorite ? Text("대표 계좌",
+                    style: TextStyle(
+                      color: color1,
+                      fontSize:20,
+                      fontWeight: FontWeight.w600
+                    ),
+                  ) : SizedBox.shrink(),
+                  Text(widget.account.accountAlias!,
+                    style: TextStyle(
+                      fontSize: widget.isFavorite ? 20 : 17,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
-                ) : SizedBox.shrink(),
-                Text(widget.nickName,
-                  style: TextStyle(
-                    fontSize: widget.isFavorite ? 20 : 17,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                Row(
-                  children: [
-                    Text(widget.bankName+" ",
-                      style: TextStyle(
-                        fontSize: fontsize,
-                        fontWeight: FontWeight.w500,
+                  Row(
+                    children: [
+                      Text(widget.account.bank!+" ",
+                        style: TextStyle(
+                          fontSize: fontsize,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
-                    ),
-                    viewDetail ? SizedBox.shrink(): Text(widget.accountNum,
-                      style: TextStyle(
-                        fontSize: fontsize,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    )
-                  ],
-                ),
-                viewDetail ? Text(widget.accountNum,
-                  style: TextStyle(
-                    fontSize: fontsize,
-                    fontWeight: FontWeight.w500,
+                      viewDetail ? SizedBox.shrink(): Text(widget.account.accountNum!.toString(),
+                        style: TextStyle(
+                          fontSize: fontsize,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      )
+                    ],
                   ),
-                ) : Divider(),
-                SizedBox(height:10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                        width:size.width*0.42,
-                        height: 60,
-                        child: OutlinedButton(
-                            onPressed: (){
-                            },
-                            style: OutlinedButton.styleFrom(
-                              backgroundColor: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                            ),
-                            child: Text("계좌 수정",
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ))
+                  viewDetail ? Text(widget.account.accountNum!.toString(),
+                    style: TextStyle(
+                      fontSize: fontsize,
+                      fontWeight: FontWeight.w500,
                     ),
-                    Container(
-                        width:size.width*0.42,
-                        height: 60,
-                        child: OutlinedButton(
-                            onPressed: (){
-                            },
-                            style: OutlinedButton.styleFrom(
-                              backgroundColor: color1,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                            ),
-                            child: Text("계좌 삭제",
-                              style: TextStyle(
-                                color:Colors.white,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ))
-                    ),
-                  ],
-                ),
-                Divider(),
+                  ) : Divider(),
+                  SizedBox(height:10),
 
-              ],
+
+                ],
+              ),
             ),
-          ),
+            Container(
+              height: 80,
+              decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.7),
+                    ),
+                    const BoxShadow(
+                      color: Color(0xFFF4F4F4),
+                      spreadRadius: -2.0,
+                      blurRadius: 4.0,
+                    )
+                  ]
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                      width:size.width*0.44,
+                      height: 55,
+                      margin: EdgeInsets.only(left:15),
+                      child: OutlinedButton(
+                          onPressed: (){
+                            provider.accounts.remove(widget.account);
+                            provider.accounts.insert(0,widget.account);
+
+                          },
+                          style: OutlinedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            side: const BorderSide(
+                              color: Color(0xFFD9D9D9),
+                            ),
+                          ),
+                          child: Text("계좌 수정",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ))
+                  ),
+                  Container(
+                      width:size.width*0.44,
+                      height: 55,
+                      margin: EdgeInsets.only(right:15),
+                      child: OutlinedButton(
+                          onPressed: (){
+                          },
+                          style: OutlinedButton.styleFrom(
+                            backgroundColor: color1,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            side: const BorderSide(
+                              color: Colors.transparent
+                            ),
+                          ),
+                          child: Text("계좌 삭제",
+                            style: TextStyle(
+                              color:Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ))
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
